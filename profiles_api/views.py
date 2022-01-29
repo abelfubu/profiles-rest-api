4a98a7f80cf318e2db3a71da0d84b7aff15a059f
Abel de la Fuente <abel.delafuente@feverup.com>
2022-01-29 11:14:44 +0100

    feat(profiles): add http methods to the API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -13,4 +17,39 @@ class HelloApiView(APIView):
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs',
         ]
+
         return Response({'message': 'Hello', 'data': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': f'Hello {name}'})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(sels, request, pk=None):
+        """Handle updating an object"""
+        return Response(
+            {'method': 'PUT'},
+            status=status.HTTP_200_OK
+        )
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response(
+            {'method': 'PATCH'},
+            status=status.HTTP_200_OK
+        )
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response(
+            {'method': 'DELETE'},
+            status=status.HTTP_200_OK
+        )
